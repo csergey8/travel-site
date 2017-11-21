b57a13f2ace3d7d20443f5bd3749c391f6ed663e
csergey8 <c.sergey@protonmail.com>
2017-11-21 17:06:49 +0200

    install browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 
-
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var postcss = require('gulp-postcss');
@@ -7,6 +6,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     console.log('!!!')
@@ -24,12 +24,25 @@ gulp.task('styles', function(){
 
 gulp.task('watch', function() {
 
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        },
+        notify: false
+    });
+
     watch('./app/index.html', function() {
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
 
+});
+
+
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
