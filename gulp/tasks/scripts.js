625ea804d962a18293b770f0ed4c58d5648fa16a
csergey8 <c.sergey@protonmail.com>
2017-11-26 09:11:56 +0200

    Completed huge mobile menu

---
+++ gulp/tasks/scripts.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var webpack = require('webpack');
+
+gulp.task('scripts', function(callback) {
+    webpack(require('../../webpack.config.js'), function(err, stats){
+        if (err) {
+            console.log('!!!!!MY ERRR' + err.toString());
+        }
+        console.log('webpack is run');
+        console.log(stats.toString());
+        callback();
+    });
+});
