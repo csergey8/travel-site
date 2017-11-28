83a7b0657873351033c84595517af53f8c32f971
csergey8 <c.sergey@protonmail.com>
2017-11-28 16:40:28 +0200

    completed gulp build task

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 var path = require('path');
 
+
 module.exports = {
     entry: {
-        App: "./app/assets/scripts/App.js",
-        Vendor: "./app/assets/scripts/Vendor.js"
+      App: "./app/assets/scripts/App.js",
+      Vendor: "./app/assets/scripts/Vendor.js"
     },
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"),
-        filename: "[name].js"
+      filename: "[name].js"
     },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /[node_modules]/,
-                loader: 'babel-loader'
-            }
-        ]
+
+        
+      loaders: [
+        {
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015']
+          },
+          test: /\.js$/,
+          exclude: /node_modules/
+        }
+      ]
     }
-}
+  }
